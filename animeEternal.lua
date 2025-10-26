716f6612a4b08bf76426a79362c756fe9059a632
LightningIWNL <flukzfl@gmail.com>
2025-10-27 06:01:08 +0700

    1.0

---
+++ animeEternal.lua
@@ -0,0 +1,524 @@
+-- UI Library
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet(
+"https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+
+
+--Variable
+local Collection = {}; Collection.__index = Collection
+local Players = game:GetService("Players")
+local GuiService = game:GetService("GuiService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local VirtualInputManager = game:GetService("VirtualInputManager")
+local UserInputService = game:GetService("UserInputService")
+local LocalPlayer = Players.LocalPlayer
+local PlayerGui = LocalPlayer:WaitForChild("PlayerGui")
+local CoreGui = game:GetService("CoreGui")
+local ScreenGui = Instance.new("ScreenGui")
+local ImageButton = Instance.new("ImageButton")
+local dragging = false
+local startPos
+local startMousePos
+local Events = ReplicatedStorage:WaitForChild("Events")
+local Inventory = Events:WaitForChild("Inventory")
+local To_Server = Events:WaitForChild("To_Server")
+local selectedList = {}
+local autofarm = false
+local autoRankUp = false
+local inDungeon = false
+local Dungeon_Notification = PlayerGui.Dungeon.Dungeon_Notification
+local Dungeon_Header = PlayerGui.Dungeon.Default_Header
+local autoFarmAll = false
+local Monsters = workspace:WaitForChild("Debris"):WaitForChild("Monsters")
+local entitiesName, seen = {}, {}
+local currentMonsters = {}
+for _, Entity in pairs(workspace.Debris.Monsters:GetChildren()) do
+    local title = Entity:GetAttribute("Title")
+    if title and not seen[title] then
+        table.insert(entitiesName, title)
+        seen[title] = true
+    end
+end
+
+
+local statName = {
+    "Primary_Damage",
+    "Primary_Energy",
+    "Primary_Coins",
+    "Primary_Luck",
+}
+
+
+
+-------------------------------------------------------------------------------------------
+-----------------------------------------Function------------------------------------------
+-------------------------------------------------------------------------------------------
+
+function Collection:selectAutoFarm()
+    if not autofarm then
+        return
+    end
+    if autofarm then
+        task.spawn(function()
+            while autofarm do
+                if #selectedList > 0 then
+                    local closest = Collection:getEntities(selectedList)
+                    if closest then
+                        if Collection:GetSelfDistance(closest.HumanoidRootPart.Position) > 7 then
+                            Collection:TeleportCFrame(
+                                closest.HumanoidRootPart.CFrame
+                                * CFrame.new(0, 0, -5)
+                                * CFrame.Angles(0, math.rad(180), 0)
+                            )
+                        end
+                        Collection:attackEntity(tostring(closest))
+                    end
+                end
+                task.wait()
+            end
+        end)
+    end
+end
+
+local function stopSelectEntities(reason)
+    if autofarm then
+        autofarm = false
+        print("[SelectEntities] stopped:", reason or "unknown")
+    end
+end
+
+function Collection:AutoClaimChest()
+    To_Server:FireServer({
+        Action = "_Chest_Claim",
+        Name = "Group"
+    })
+end
+
+function Collection:GetRoot(Character)
+    return Character:FindFirstChild("HumanoidRootPart")
+end
+
+function Collection:GetSelfDistance(Position)
+    local RootPart = Collection:GetRoot(LocalPlayer.Character)
+    return (RootPart.Position - Position).Magnitude
+end
+
+function Collection:TeleportCFrame(Position)
+    local RootPart = Collection:GetRoot(LocalPlayer.Character)
+    RootPart.CFrame = typeof(Position) == "CFrame" and Position or CFrame.new(Position)
+end
+
+function Collection:attackEntity(entityID)
+    To_Server:FireServer({
+        Id = entityID,
+        Action = "_Mouse_Click"
+    })
+end
+
+function Collection:getEntities(Entities)
+    local distanceData = {}
+    local entitiesData = {}
+    local entities = {}
+
+
+    local RootPart = Collection:GetRoot(LocalPlayer.Character)
+
+    for _, Entity in pairs(Monsters:GetChildren()) do
+        local title = Entity:GetAttribute("Title")
+        if table.find(Entities, Entity:GetAttribute("Title")) then
+            local distance = math.floor((Entity["HumanoidRootPart"].Position - RootPart.Position).Magnitude)
+
+            table.insert(entities, Entity)
+            table.insert(distanceData, distance)
+            entitiesData[tostring(distance)] = Entity
+        end
+    end
+
+    if #distanceData <= 0 then return nil, nil end
+
+    return entitiesData[tostring(math.min(unpack(distanceData)))], entities
+end
+
+function Collection:getSkillPoints()
+    local txt = PlayerGui.PlayerHUD.Player_Hub.Primary_Stats.Stats.Frame.Skill_Points.Text
+    local digits = string.match(txt or "", "%d+")
+    return tonumber(digits) or 0
+end
+
+function Collection:autoUpRank()
+    To_Server:FireServer({
+        Upgrading_Name = "Rank",
+        Action = "_Upgrades",
+        Upgrade_Name = "Rank_Up"
+    })
+end
+
+function Collection:getAllEntities()
+    local EntitiesName = {}
+    for _, Entity in pairs(Monsters:GetChildren()) do
+        local title = Entity:GetAttribute("Title")
+        if title and not table.find(EntitiesName, title) then
+            table.insert(EntitiesName, title)
+        end
+    end
+    return EntitiesName
+end
+
+function Collection:autoFarmAll()
+    autoFarmAll = true
+    autoFarmAll = task.spawn(function()
+        while autoFarmAll do
+            local allTitles = Collection:getAllEntities()
+            local closestEntity, allEntities = Collection:getEntities(allTitles)
+            if closestEntity then
+                if Collection:GetSelfDistance(closestEntity["HumanoidRootPart"].Position) > 7 and Collection:GetSelfDistance(closestEntity["HumanoidRootPart"].Position) < 500 then
+                    Collection:TeleportCFrame(closestEntity["HumanoidRootPart"].CFrame * CFrame.new(0, 0, -5) *
+                    CFrame.Angles(0, math.rad(180), 0))
+                end
+                Collection:attackEntity(tostring(closestEntity))
+            end
+            task.wait()
+        end
+    end)
+end
+
+function Collection:updateEntitiesName()
+    local newEntitiesName = {}
+    local newSeen = {}
+
+    for _, Entity in pairs(workspace.Debris.Monsters:GetChildren()) do
+        local title = Entity:GetAttribute("Title")
+        if title and not newSeen[title] then
+            table.insert(newEntitiesName, title)
+            newSeen[title] = true
+        end
+    end
+
+    return newEntitiesName
+end
+
+--------------------------------------------------------------------------------------------
+-----------------------------------------UI Setup------------------------------------------
+--------------------------------------------------------------------------------------------
+ScreenGui.Parent = CoreGui
+ScreenGui.Name = "FleXiZ"
+ImageButton.Size = UDim2.fromOffset(128, 128)
+ImageButton.Position = UDim2.new(0.5, -ImageButton.Size.X.Offset / 2, 0, 10) -- มุมขวาล่าง
+ImageButton.BackgroundTransparency = 1                                     -- โปร่งใส ไม่มีพื้นหลัง
+ImageButton.Image = "rbxassetid://136992027589423"                         -- ใส่ ID รูปที่อยากใช้
+ImageButton.ImageColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.Parent = ScreenGui
+
+local Window = Fluent:CreateWindow({
+    Title = "FleXiZ " .. Fluent.Version,
+    SubTitle = "by FleXiZ",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true,
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl
+})
+
+ImageButton.MouseButton1Click:Connect(function()
+    if Window.Minimize then
+        Window.Minimize(false)
+    end
+end)
+
+ImageButton.InputBegan:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 then
+        dragging = true
+        startPos = ImageButton.Position
+        startMousePos = input.Position
+    end
+end)
+
+
+ImageButton.InputEnded:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 then
+        dragging = false
+    end
+end)
+UserInputService.InputChanged:Connect(function(input)
+    if dragging and input.UserInputType == Enum.UserInputType.MouseMovement then
+        local delta = input.Position - startMousePos
+        ImageButton.Position = UDim2.new(
+            startPos.X.Scale,
+            startPos.X.Offset + delta.X,
+            startPos.Y.Scale,
+            startPos.Y.Offset + delta.Y
+        )
+    end
+end)
+
+local Tabs = {
+    General = Window:AddTab({ Title = "General", Icon = "monitor" }),
+    Dungeon = Window:AddTab({ Title = "Dungeon", Icon = "shield" }),
+    Stats = Window:AddTab({ Title = "Stats", Icon = "align-end-horizontal" }),
+    Reward = Window:AddTab({ Title = "Reward", Icon = "trophy" }),
+}
+
+task.spawn(function()
+    while task.wait(.5) do
+        if Fluent.Unloaded then
+            if ImageButton and ImageButton.Parent then
+                ImageButton:Destroy()
+            end
+            break
+        end
+    end
+end)
+
+
+-------------------------------------------------------------------------------------------
+-----------------------------------------General Tab-----------------------------------------
+-------------------------------------------------------------------------------------------
+
+local AntiAFK = false
+local Toggle = Tabs.General:AddToggle("MyToggle", { Title = "Anti AFK", Default = true })
+Toggle:OnChanged(function(Toggle)
+    AntiAFK = Toggle
+    task.spawn(function()
+        while AntiAFK do
+            print("Anti AFK")
+            VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Space, false, game)
+            VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Space, false, game)
+            task.wait(60 * 14)
+        end
+    end)
+end)
+
+local Toggle = Tabs.General:AddToggle("MyToggle", { Title = "Auto Rank Up", Default = false })
+Toggle:OnChanged(function(Toggle)
+    autoRankUp = Toggle
+    task.spawn(function()
+        while autoRankUp do
+            print("Auto Up Rank")
+            Collection:autoUpRank()
+            task.wait(30)
+        end
+    end)
+end)
+
+
+
+local MultiDropdown = Tabs.General:AddDropdown("MultiDropdown", {
+    Title = "Select Entities",
+    Values = entitiesName,
+    Multi = true,
+    Default = { "" },
+    Description = "This Function will attack selected entities automatically"
+})
+
+
+
+
+
+MultiDropdown:OnChanged(function(select)
+    selectedList = {}
+    for i, v in pairs(select) do
+        if v then
+            table.insert(selectedList, i)
+        end
+    end
+    autofarm = true
+    Collection:selectAutoFarm()
+end)
+
+task.spawn(function()
+    while task.wait(2) do
+        local oldCount = #entitiesName
+        entitiesName = Collection:updateEntitiesName()
+
+        if oldCount ~= #entitiesName then
+            MultiDropdown:SetValues(entitiesName)
+            print("Auto-refreshed entities: " .. #entitiesName .. " monsters")
+        end
+    end
+end)
+
+--------------------------------------------------------------------------------------------
+-----------------------------------------Dungeon Tab-----------------------------------------
+--------------------------------------------------------------------------------------------
+local DungeonName = {
+    "Dungeon_Easy",
+    "Dungeon_Medium",
+    "Dungeon_Hard",
+    "Dungeon_Insane",
+    "Dungeon_Crazy",
+    "Dungeon_Nightmare",
+}
+
+function Collection:joinDungeon(dungeonName)
+    To_Server:FireServer({
+        Action = "_Enter_Dungeon",
+        Name = dungeonName
+    })
+end
+
+local MultiDropdown = Tabs.Dungeon:AddDropdown("MultiDropdown", {
+    Title = "Select Dungeons",
+    Values = DungeonName,
+    Multi = true,
+    Default = {},
+    Description = "This Function will attack selected entities automatically"
+})
+
+
+local dungeonList = {}
+local autoDungeon = false
+MultiDropdown:OnChanged(function(select)
+    dungeonList = {}
+    for i, v in pairs(select) do
+        if v then
+            table.insert(dungeonList, i)
+        end
+    end
+
+    if not autoDungeon then
+        autoDungeon = true
+        task.spawn(function()
+            while autoDungeon do
+                if Dungeon_Notification.Visible then
+                    local m = tonumber(os.date("%M"))
+                    local s = tonumber(os.date("%S"))
+                    for _, dungeon in pairs(dungeonList) do
+                        if (m == 00 and s == 20) and dungeon == "Dungeon_Easy" then
+                            Dungeon_Notification.Visible = false
+                            Collection:joinDungeon(dungeon)
+                            inDungeon = true
+                            stopSelectEntities("entering dungeon: " .. tostring(dungeon))
+                            task.wait(.5)
+                            autoFarmAll = true
+                            Collection:autoFarmAll()
+                        elseif (m == 10 and s == 20) and dungeon == "Dungeon_Medium" then
+                            Dungeon_Notification.Visible = false
+                            Collection:joinDungeon(dungeon)
+                            inDungeon = true
+                            stopSelectEntities("entering dungeon: " .. tostring(dungeon))
+                            task.wait(.5)
+                            autoFarmAll = true
+                            Collection:autoFarmAll()
+                        elseif (m == 20 and s == 20) and dungeon == "Dungeon_Hard" then
+                            Dungeon_Notification.Visible = false
+                            Collection:joinDungeon(dungeon)
+                            inDungeon = true
+                            stopSelectEntities("entering dungeon: " .. tostring(dungeon))
+                            task.wait(.5)
+                            autoFarmAll = true
+                            Collection:autoFarmAll()
+                        elseif (m == 30 and s == 20) and dungeon == "Dungeon_Insane" then
+                            Dungeon_Notification.Visible = false
+                            Collection:joinDungeon(dungeon)
+                            inDungeon = true
+                            stopSelectEntities("entering dungeon: " .. tostring(dungeon))
+                            task.wait(.5)
+                            autoFarmAll = true
+                            Collection:autoFarmAll()
+                        elseif (m == 40 and s == 20) and dungeon == "Dungeon_Crazy" then
+                            Dungeon_Notification.Visible = false
+                            Collection:joinDungeon(dungeon)
+                            inDungeon = true
+                            stopSelectEntities("entering dungeon: " .. tostring(dungeon))
+                            task.wait(.5)
+                            autoFarmAll = true
+                            Collection:autoFarmAll()
+                        elseif (m == 50 and s == 20) and dungeon == "Dungeon_Nightmare" then
+                            Dungeon_Notification.Visible = false
+                            Collection:joinDungeon(dungeon)
+                            inDungeon = true
+                            stopSelectEntities("entering dungeon: " .. tostring(dungeon))
+                            task.wait(.5)
+                            autoFarmAll = true
+                            Collection:autoFarmAll()
+                        end
+                    end
+                else
+                    if inDungeon and not Dungeon_Header.Visible then
+                        inDungeon = false
+                        autofarm = true
+                        autoFarmAll = false
+                        Collection:selectAutoFarm()
+                    end
+                end
+                task.wait()
+            end
+        end)
+    end
+end)
+-------------------------------------------------------------------------------------------
+-----------------------------------------Stats Tab-----------------------------------------
+-------------------------------------------------------------------------------------------
+
+local MultiDropdown = Tabs.Stats:AddDropdown("MultiDropdown", {
+    Title = "Select Entities",
+    Values = statName,
+    Multi = true,
+    Default = { "" },
+    Description = "This Function will attack selected entities automatically"
+})
+
+local selectedStatList = {}
+local upgradeStats = false
+MultiDropdown:OnChanged(function(selectStat)
+    selectedStatList = {}
+    for i, v in pairs(selectStat) do
+        if v then
+            table.insert(selectedStatList, i)
+        end
+    end
+
+
+    if not upgradeStats then
+        upgradeStats = true
+        task.spawn(function()
+            while upgradeStats do
+                local skillPoints = Collection:getSkillPoints()
+                if skillPoints >= 1 then
+                    for _, stat in pairs(selectedStatList) do
+                        To_Server:FireServer({
+                            Name = stat,
+                            Action = "Assign_Level_Stats",
+                            Amount = 1
+                        })
+                        print("Upgraded Stat: " .. stat)
+                        task.wait(0.1)
+                    end
+                end
+                task.wait()
+            end
+        end)
+    end
+end)
+
+-------------------------------------------------------------------------------------------
+-----------------------------------------Reward Tab-----------------------------------------
+-------------------------------------------------------------------------------------------
+local function ChestToggle(key, title, chestName)
+    local isOn = false
+
+    local ToggleObj = Tabs.Reward:AddToggle(key, { Title = title, Default = false })
+
+    ToggleObj:OnChanged(function(state)
+        isOn = state
+        if isOn then
+            task.spawn(function()
+                pcall(function()
+                    To_Server:FireServer({
+                        Action = "_Chest_Claim",
+                        Name = chestName
+                    })
+                end)
+            end)
+        end
+    end)
+end
+
+ChestToggle("DailyChestToggle", "Claim Daily Chest", "Daily")
+ChestToggle("GroupChestToggle", "Claim Group Chest", "Group")
+ChestToggle("VipChestToggle", "Claim VIP Chest", "Vip")
+ChestToggle("PremiumChestToggle", "Claim Premium Chest", "Premium")
+
+--------------------------------------------------------------------------------------------
+-----------------------------------------Loops----------------------------------------------
+--------------------------------------------------------------------------------------------
